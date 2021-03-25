e9d9048b97ff879e35770b76e89f29097268f262
pyrus-vagus <grushadaria@gmail.com>
2021-03-25 05:27:26 +0300

    feat: implement function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
+const { FakeXMLHttpRequest } = require("sinon");
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity !== /^[1-9]$/g) {
+    return false;
+  } else {
+    const a = MODERN_ACTIVITY / +sampleActivity;
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    const time = Math.log(a) / k;
+    return Math.ceil(time);
+  }
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return members
+    .map((i) => (typeof i === "string" ? i.split("").shift() : ""))
+    .sort()
+    .join("");
 };
