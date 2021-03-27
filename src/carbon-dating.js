98a688d5ef56de4912c9c8ce27efb07386d50ad7
pyrus-vagus <grushadaria@gmail.com>
2021-03-27 19:16:55 +0300

    feat: implement solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,16 +5,10 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (
-    +sampleActivity > 15 ||
-    +sampleActivity < 0 ||
-    sampleActivity === "Infinity" ||
-    +sampleActivity === Number.NEGATIVE_INFINITY ||
-    typeof sampleActivity !== "string" ||
-    isNaN(sampleActivity)
-  ) {
-    return false;
-  } else {
+  if (sampleActivity <= 0 || sampleActivity >= MODERN_ACTIVITY) return false;
+  if (typeof sampleActivity !== "string") return false;
+  if (isNaN(sampleActivity)) return false;
+  else {
     const a = MODERN_ACTIVITY / +sampleActivity;
     const k = 0.693 / HALF_LIFE_PERIOD;
     const time = Math.log(a) / k;
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
+const { captureStackTrace } = require("../extensions/custom-error");
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let arrDepth = 1;
+    arr.forEach((i) => {
+      if (Array.isArray(i)) {
+        const aquiredDepth = this.calculateDepth(i) + 1;
+        if (arrDepth < aquiredDepth) {
+          arrDepth = aquiredDepth;
+        }
+      }
+    });
+    return arrDepth;
   }
 };
