0b8575c9da5dc64c55d2806d0f9d0f0fd2f3a89e
pyrus-vagus <grushadaria@gmail.com>
2021-03-27 21:24:09 +0300

    add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    console.log(value);
+    value !== "undefined"
+      ? this.chain.push(`( ${value} )`)
+      : this.chain.push(`( )`);
+    return chainMaker;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position) {
+      this.chain.splice(position - 1, 1);
+    } else {
+      this.chain = [];
+      throw new Error("Position is not valid");
+    }
+
+    return chainMaker;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return chainMaker;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let a = this.chain.join("~~");
+    this.chain = [];
+    return a;
+  },
 };
 
 module.exports = chainMaker;
--- src/what-season.js
+++ src/what-season.js
@@ -1,44 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if (!date) {
-    return "Unable to determine the time of year!";
-  } else {
-    function isValid(d) {
-      return Object.prototype.toString.call(d) === "[object Date]" && !isNaN;
-    }
-    function check(d) {
-      if (!isValid(d)) {
-        throw new RangeError("invalid date");
-      }
-    }
-    try {
-      check(date);
-    } catch (e) {
-      if (e instanceof RangeError) {
-      }
-    }
-    switch (date.getMonth()) {
-      case 11:
-      case 0:
-      case 1:
-        return "winter";
-        break;
-      case 2:
-      case 3:
-      case 4:
-        return "spring";
-        break;
-      case 5:
-      case 6:
-      case 7:
-        return "summer";
-        break;
-      case 8:
-      case 9:
-      case 10:
-        return "autumn";
-        break;
-    }
+  if (!date) return "Unable to determine the time of year!";
+
+  if (Object.prototype.toString.call(date) !== "[object Date]")
+    throw new Error("wrong");
+
+  switch (date.getMonth()) {
+    case 11:
+    case 0:
+    case 1:
+      return "winter";
+      break;
+    case 2:
+    case 3:
+    case 4:
+      return "spring";
+      break;
+    case 5:
+    case 6:
+    case 7:
+      return "summer";
+      break;
+    case 8:
+    case 9:
+    case 10:
+      return "autumn";
+      break;
   }
 };
