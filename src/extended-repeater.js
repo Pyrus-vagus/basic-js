3b8a8f45be5c933528f1d345d2b586099b556c3a
pyrus-vagus <grushadaria@gmail.com>
2021-03-30 00:25:43 +0300

    implement solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError("Not implemented");
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|",
+  } = options;
+  const string = [];
+  const addit = [];
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    addit.push(`${addition}`);
+  }
+  const add = addit.join(additionSeparator);
+  for (let i = 0; i < repeatTimes; i++) {
+    string.push(str + add);
+  }
+  return string.join(separator);
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,6 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new TypeError("not array");
-  } else {
-    const i = arr.map((i) => i);
-    arr.map((item) => {
-      item === "--discard-next"
-        ? i.splice(i.indexOf("--discard-next") + 1, 2)
-        : item === "--discard-prev"
-        ? i.splice(i.indexOf("--discard-prev") - 1, 2)
-        : item === "--double-next"
-        ? (i[i.indexOf(item)] = i[i.indexOf(item) + 1])
-        : item === "--double-prev"
-        ? (i[i.indexOf(item)] = i[i.indexOf(item) - 1])
-        : item;
-    });
-    return i;
   }
+  const finalArray = [];
 };
