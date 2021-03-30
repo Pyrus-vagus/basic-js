457e293541e426a446900564f57c7079b75e9860
pyrus-vagus <grushadaria@gmail.com>
2021-03-31 02:10:51 +0300

    implement solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error("no parameters");
+    let cipher = "";
+    let count = 0;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        cipher += String.fromCharCode(
+          ((message.charCodeAt(i) + key.charCodeAt(count % key.length) - 130) %
+            26) +
+            65
+        );
+        count++;
+      } else cipher += message[i];
+    }
+
+    if (this.isDirect) {
+      return cipher;
+    } else {
+      return cipher.split("").reverse().join("");
+    }
+  }
+  decrypt(message, key) {
+    if (!message || !key) throw new Error("no parameters");
+
+    let cipher = "";
+    let count = 0;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        cipher += String.fromCharCode(
+          ((message.charCodeAt(i) + 26 - key.charCodeAt(count % key.length)) %
+            26) +
+            65
+        );
+        count++;
+      } else cipher += message[i];
+    }
+
+    if (this.isDirect) {
+      return cipher;
+    } else {
+      return cipher.split("").reverse().join("");
+    }
   }
 }
 
